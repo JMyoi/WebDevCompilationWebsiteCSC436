2d6188218629d601db4d45e1ae35de8fabdf5a21
Jay Chen <jaychenjjc@gmail.com>
2025-11-04 23:38:26 -0500

    header working

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return(
+        <div className = "flex flex-col bg-stone-100">
+            <h1 className = ""> CSC436 Compilation Webiste</h1>
+            <p className = " ">A compilation of all websites created by Adam Kostandy, teaching modern web development</p>
+        </div>
+    )
+}
+
+export default Header;
