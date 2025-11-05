ca2648c5bccfec117c797a76f5cb3dad3978e306
Jay Chen <jaychenjjc@gmail.com>
2025-11-05 18:47:21 -0500

    svg to png

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer(){
     return(
       <div className = "flex flex-col items-center p-3  bg-gradient-to-r from-violet-400 to-sky-400 py-6">
         <a target="_blank" href = "https://github.com/JMyoi/WebDevCompilationWebsiteCSC436">
-            <img src = "../src/assets/github.svg" alt = "githubLink"/>
+            <img src = "../src/assets/github.png" alt = "githubLink"/>
         </a>
         <p className = "pt-3 text-white">@2025 Jay Chen</p>
         
