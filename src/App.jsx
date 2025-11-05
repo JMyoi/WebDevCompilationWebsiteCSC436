e6cab2c4ea849ff24374a54934d9eb7428aed752
Jay <127246162+JMyoi@users.noreply.github.com>
2025-11-05 16:43:38 -0500

    change to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header'
-import SiteCard from './components/SiteCard';
+import SiteCard from './components/SiteCard.jsx';
 import Footer from './components/Footer';
 
 function App() {
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ function Header() {
     return(
         <div className = "flex flex-col items-center p-3  bg-gradient-to-r from-violet-400 to-sky-400 "> 
             <h1 className = "text-5xl m-2"> CSC436 Compilation Webiste</h1> 
-            <p className = "m-2">A compilation of all websites created by {" "}
+            <p className = "m-2">A compilation of all website created by {" "}
                 <a target="_blank" href = "https://github.com/diabeatz96" className = "cursor-pointer italic hover:animate-pulse hover:text-yellow-300">
                     Adam Kostandy
                 </a>
-                , used teaching modern web development
+                , used for teaching Modern Web Development
             </p>
         </div>
     )
