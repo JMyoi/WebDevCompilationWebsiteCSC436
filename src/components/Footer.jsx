35296508ed4786493a85f3aca5e154430e7686e2
Jay <127246162+JMyoi@users.noreply.github.com>
2025-11-05 17:03:44 -0500

    logo Change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer(){
     return(
       <div className = "flex flex-col items-center p-3  bg-gradient-to-r from-violet-400 to-sky-400 py-6">
         <a target="_blank" href = "https://github.com/JMyoi/WebDevCompilationWebsiteCSC436">
-            <img src = "src/assets/github-mark-white.svg"/>
+            <img src = "../src/assets/github.svg" alt = "githubLink"/>
         </a>
         <p className = "pt-3 text-white">@2025 Jay Chen</p>
         
